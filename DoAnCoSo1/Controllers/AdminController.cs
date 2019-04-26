46d6a335f9cec7874388a39156765df194704d22
kuli098 <kuli098@cntt.github.com>
2019-04-26 12:27:34 +0700

    Admin

---
+++ DoAnCoSo1/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnCoSo1.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
