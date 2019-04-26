d728ed13e9a564b069ce5d9906d84cf7f9b252cb
Long <hutvobalong@gmail.com>
2019-04-26 17:32:38 +0700

    Copy từ Long rồi Paste vào Master

---
+++ DoAnCoSo1/Models/Tintuc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoAnCoSo1.Models
+{
+    public partial class Tintuc
+    {
+        public int MaTintuc { get; set; }
+        public string Tieude { get; set; }
+        public string Noidung { get; set; }
+        public string MaTheloai { get; set; }
+        public DateTime? Ngaydang { get; set; }
+
+        public Theloai MaTheloaiNavigation { get; set; }
+    }
+}
